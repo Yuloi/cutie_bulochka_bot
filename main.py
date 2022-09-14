6789a7046a6335acc691d2b6b6b0fa82a891dc27
Yuloi <viegocat@yandex.ru>
2022-09-14 22:56:19 +0300

    changed variables

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ bot = telebot.TeleBot(token)
 bulochka_in = True
 cutie_in = True
 bulochka = '\U0001f408\u200D\u2B1BBulochka'
-bulochka_outside = open('images/bul_out.mp4', 'rb')
-cutie_outside = open('images/cutie_out.mp4', 'rb')
+bulochkaOutsideVideo = open('images/bul_out.mp4', 'rb')
+cutieOutsideVideo = open('images/cutie_out.mp4', 'rb')
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -21,10 +21,12 @@ def start(message):
     markup.add(item1, item2, item3)
     bot.send_message(message.chat.id, 'Hi,{0.first_name}'.format(message.from_user), reply_markup=markup)
 
+@bot.message_handler(content_types=['text', 'video', 'images'])
+
 
 @bot.message_handler(content_types=['text', 'video', 'images'])
 def bot_message(message):
-    global bulochka_in, cutie_in, bulochka, bulochka_outside, cutie_outside
+    global bulochka_in, cutie_in, bulochka, bulochkaOutsideVideo, cutieOutsideVideo
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     item1 = types.KeyboardButton('Check status')
     item1a = types.KeyboardButton('Check status again')
@@ -63,7 +65,7 @@ def bot_message(message):
             bulochka_in = False
             bot.send_message(message.chat.id, 'Bulochka\'s status was updated, Enjoy your walking, Bulochka!',
                              reply_markup=markup)
-            bot.send_video(message.chat.id, bulochka_outside)
+            bot.send_video(message.chat.id, bulochkaOutsideVideo)
         elif message.text == '\U0001f408\u200D\u2B1B\U0001f3e0Bulochka is inside':
             markup.add(item1, back)
             bulochka_in = True
@@ -74,7 +76,7 @@ def bot_message(message):
             cutie_in = False
             bot.send_message(message.chat.id, '\U0001F431 Cutie\'s status was updated, Enjoy your walking, Cutie!',
                              reply_markup=markup)
-            bot.send_video(message.chat.id, cutie_outside)
+            bot.send_video(message.chat.id, cutieOutsideVideo)
         elif message.text == '\U0001F431\U0001f3e0Cutie is inside':
             markup.add(item1, back)
             cutie_in = True
